a96091e82c7e94ff8030d4958e788208722f94e8
Jeff Hall <jeff.leon.hall@gmail.com>
2021-01-10 20:07:57 -0700

    generalized this function

--- words.py
+++ words.py
@@ -1,8 +1,12 @@
-def print_upper_words(list):
-    """capitalize each word and print"""
+def print_upper_words(list, must_start_with):
+    """Capitalize and print each word that starts with 'e'."""
 
     for word in list:
-        if word.upper().startswith('E') == True:
+        if word[0] in must_start_with:
             print(word.upper() + '\n')
 
-print_upper_words(['bob','enormous','glory', 'name', 'entertain', '34df', 'Everywhere'])
+print('Should print BOB and GLORY \n')
+print_upper_words(['bob','enormous','glory', 'name', 'entertain', '34df', 'Everywhere'], must_start_with={'g', 'b'})
+
+print('Should print ZOOKEEPER \n')
+print_upper_words(['banana', 'Cat', 'dog', 'angel', 'zookeeper', 'facemask'], {'v', 'z'})
