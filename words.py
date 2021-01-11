8835b206428411bcddb2bbb3af095225f27bdfe6
Jeff Hall <jeff.leon.hall@gmail.com>
2021-01-10 19:48:45 -0700

    modified print_upper_words to only print words that start with e

--- words.py
+++ words.py
@@ -2,4 +2,7 @@ def print_upper_words(list):
     """capitalize each word and print"""
 
     for word in list:
-        print(word.upper() + '\n')
+        if word.upper().startswith('E') == True:
+            print(word.upper() + '\n')
+
+print_upper_words(['bob','enormous','glory', 'name', 'entertain', '34df', 'Everywhere'])
